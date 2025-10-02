6b02d1cd57b8589ceeb244ec18ae957f9cc62474
rmm374 <risha.makhijani@gmail.com>
2025-10-02 17:10:43 -0400

    anything

---
+++ client.py
@@ -0,0 +1,40 @@
+import threading
+import time
+import random
+
+import socket
+def client():
+    try:
+        cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[C]: Client socket created")
+    except socket.error as err:
+        print('socket open error: {} \n'.format(err))
+        exit()
+        
+    # Define the port on which you want to connect to the server
+    port = 30068
+    localhost_addr = socket.gethostbyname(socket.gethostname())
+
+    # connect to the server on local machine
+    server_binding = (localhost_addr, port)
+    cs.connect(server_binding)
+
+    # Receive data from the server
+    data_from_server=cs.recv(100)
+    print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+
+    # close the client socket
+    cs.close()
+    exit()
+
+if __name__ == "__main__":
+    t1 = threading.Thread(name='server', target=server)
+    t1.start()
+
+    time.sleep(random.random() * 5)
+    t2 = threading.Thread(name='client', target=client)
+    t2.start()
+
+    time.sleep(5)
+    print("Done.")
+
