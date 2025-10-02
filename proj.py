6e3335c81835f0e73b58ee2a76275f8cc51513f7
rmm374 <risha.makhijani@gmail.com>
2025-10-02 17:39:20 -0400

    blahh

--- proj.py
+++ proj.py
@@ -58,9 +58,9 @@ if __name__ == "__main__":
     t1 = threading.Thread(name='server', target=server)
     t1.start()
 
-    time.sleep(random.random() * 5)
+    
     t2 = threading.Thread(name='client', target=client)
     t2.start()
 
-    time.sleep(5)
+    
     print("Done.")
